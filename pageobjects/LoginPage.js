df76aaaec24825ba32f900d71c94781f1eeb95ba
simandi_mac <srinivasaimandi@gmail.com>
2025-01-23 16:34:12 +0530

    page object session completed

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -1,12 +1,21 @@
 class LoginPage {
     constructor(page) {
         this.page = page
-        this.BASE_URL = "https://www.saucedemo.com/"
+        this.base_url = "https://www.saucedemo.com/"
+        this.iptUsername = "#user-name"
+        this.iptPassword = "#password"
+        this.btnLogin = "#login-button"
     }
 
     async load_app() {
-        await this.page.goto(this.BASE_URL)
+        await this.page.goto(this.base_url)
         await this.page.waitForSelector('.login_container');
+    }
+
+    async login(username, password) {
+        await this.page.type(this.iptUsername, username)
+        await this.page.type(this.iptPassword, password)
+        await this.page.click(this.btnLogin)
     }
 }
 module.exports = LoginPage
--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -38,46 +38,44 @@ describe('E2E Demo', function () {
         await loginPage.load_app()
 
         // fill login form and submit the form
-        await page.type("#user-name", "standard_user")
-        await page.type("#password", "secret_sauce")
-        await page.click("#login-button")
-        await setTimeout(3000)
+        await loginPage.login("standard_user", "secret_sauce")
+        await setTimeout(sleepTime)
 
         // wait for products page to load
         await page.waitForSelector(".inventory_item")
         await page.click("#item_4_title_link")
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // add item to cart
         await page.waitForSelector(".inventory_details_name")
         await page.click("#add-to-cart")
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // navigate to cart
         await page.click("[data-test='shopping-cart-link']")
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // checkout
         await page.waitForSelector(".header_secondary_container span")
         await page.click('#checkout')
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // fill checkout information and submit
         await page.type("#first-name", "Samuel")
         await page.type("#last-name", "David")
         await page.type("#postal-code", "600125")
         await page.click("#continue")
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // check overview and finishit
         await page.waitForSelector(".header_secondary_container span")
         await page.click('#finish')
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
 
         // validate order success message
         // "Thank you for your order!"
         let uploadSuccessMessage = await page.$eval("[data-test='complete-header']", span => span.innerText)
         await expect(uploadSuccessMessage).to.equal("Thank you for your order!")
-        await setTimeout(3000)
+        await setTimeout(sleepTime)
     })
 })
